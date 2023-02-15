ca1895c8f721b72bb51ccda065e9211b4dc10019
Scarlett Park <scarlettpark@Scarlettui-MacBookPro.local>
2023-02-14 20:15:21 -0500

    button onmousedown func edit

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 var button = document.querySelector('.button');
 
-button.onclick = function () {
+button.onmousedown = function (event) {
  var red = 0;
  var blue = 255;
  var green = 0;
 
  this.style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")";
+ event.preventDefault();
 };
 
+//mousedown 
+//touchstart
